a0761788c70b81909bbc4fd4d66338cc4f4bf0a8
ScidaJ <scidaj@gmail.com>
2025-01-29 00:41:58 -0500

    Added Github icon to footer, links to repo.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { Button, Heading, Label, Textarea, Toast, Toggle, Tooltip, P, A } from 'flowbite-svelte';
-	import { ClipboardCheckOutline, CloseCircleOutline } from 'flowbite-svelte-icons';
+	import { Button, Footer, Heading, Hr, Label, Textarea, Toast, Toggle, Tooltip, P, A, FooterIcon } from 'flowbite-svelte';
+	import { ClipboardCheckOutline, CloseCircleOutline, GithubSolid } from 'flowbite-svelte-icons';
 
 	let copyHtml = false;
 	let output: string[] = [];
@@ -76,75 +76,86 @@
 	}
 </script>
 
-<div>
-	<Heading tag="h2" class="text-gray-100 lg:p-2" customSize="text-4xl font-extrabold "
-		>Welcome to the Bestiary URL Converter!</Heading
-	>
-	<P class="my-4 ml-3 text-gray-300"
-		>Paste multiple urls from the <A
-			href="https://5e.tools/bestiary.html"
-			target="_blank"
-			rel="noopener noreferrer">Bestiary list</A
-		> and click the button to convert them into links to individual stat blocks. <br />Please only
-		copy and paste links that you know are safe.</P
-	>
+<div class="flex flex-col h-screen justify-between">
+    <div>
+        <Heading tag="h2" class="text-gray-100 lg:p-2" customSize="text-4xl font-extrabold "
+            >Welcome to the Bestiary URL Converter!</Heading
+        >
+        <P class="my-4 ml-3 text-gray-300"
+            >Paste multiple urls from the <A
+            href="https://5e.tools/bestiary.html"
+            target="_blank"
+            rel="noopener noreferrer">Bestiary list</A
+            > and click the button to convert them into links to individual stat blocks. <br />Please only
+            copy and paste links that you know are safe.</P
+        >
 
-	<div>
-		<Label for="url-textarea" class="mb-2 ml-3 text-gray-400">URLs</Label>
-		<Textarea
-			bind:value
-			on:input={checkInputLength}
-			id="url-textarea"
-			rows="4"
-			name="url"
-			class="ml-3 box-border w-11/12 bg-gray-700 text-gray-100 lg:w-2/5"
-		/>
-	</div>
+        <div>
+            <Label for="url-textarea" class="mb-2 ml-3 text-gray-400">URLs</Label>
+            <Textarea
+            bind:value
+            on:input={checkInputLength}
+            id="url-textarea"
+            rows="4"
+            name="url"
+            class="ml-3 box-border w-11/12 bg-gray-700 text-gray-100 lg:w-2/5"
+            />
+        </div>
 
-	<div class="mt-2 flex">
-		<div>
-			<Button class="ml-3" on:click={convert} disabled={!showStateButton}>Convert</Button>
-		</div>
+        <div class="mt-2 flex">
+            <div>
+            <Button class="ml-3" on:click={convert} disabled={!showStateButton}>Convert</Button>
+            </div>
 
-		{#if showStateButton}
-			<div>
-				<Button class="ml-3" on:click={clear} disabled={!showStateButton}
-					><CloseCircleOutline /></Button
-				>
-			</div>
-		{/if}
+            {#if showStateButton}
+            <div>
+                <Button class="ml-3" on:click={clear} disabled={!showStateButton}
+                ><CloseCircleOutline /></Button
+                >
+            </div>
+            {/if}
 
-		{#if showStateCopy}
-			<div>
-				<Button class="ml-3" on:click={copyToClipboard}><ClipboardCheckOutline /></Button>
-				<Tooltip trigger="hover">Copy to Clipboard</Tooltip>
-			</div>
+            {#if showStateCopy}
+            <div>
+                <Button class="ml-3" on:click={copyToClipboard}><ClipboardCheckOutline /></Button>
+                <Tooltip trigger="hover">Copy to Clipboard</Tooltip>
+            </div>
 
-			<div>
-				<Toggle class="ml-3 mt-2 text-gray-300" bind:checked={copyHtml}>Copy as HTML</Toggle>
-				<Tooltip trigger="hover"
-					>Copy links as HTML. For use with Google Docs and other editors.</Tooltip
-				>
-			</div>
-		{/if}
-	</div>
+            <div>
+                <Toggle class="ml-3 mt-2 text-gray-300" bind:checked={copyHtml}>Copy as HTML</Toggle>
+                <Tooltip trigger="hover"
+                >Copy links as HTML. For use with Google Docs and other editors.</Tooltip
+                >
+            </div>
+            {/if}
+        </div>
 
-	<div class="mt-1 inline-grid">
-		{#each output as url}
-			<A href={url} target="_blank" rel="noopener noreferrer" class="mb-2 ml-3 inline-block"
-				>{url}</A
-			>
-		{/each}
-	</div>
+        <div class="mt-1 inline-grid">
+            {#each output as url}
+            <A href={url} target="_blank" rel="noopener noreferrer" class="mb-2 ml-3 inline-block"
+                >{url}</A
+            >
+            {/each}
+        </div>
 
-	<div>
-		<Toast
-			dismissable={false}
-			bind:toastStatus
-			position="bottom-left"
-			class="w-44 rounded bg-primary-600"
-		>
-			<P class="text-white">Copied to Clipboard</P>
-		</Toast>
-	</div>
+        <div>
+            <Toast
+            dismissable={false}
+            bind:toastStatus
+            position="bottom-left"
+            class="w-44 rounded bg-primary-600"
+            >
+            <P class="text-white">Copied to Clipboard</P>
+            </Toast>
+        </div>
+    </div>
+
+    <div>
+        <Hr hrClass="bg-gray-500 border-gray-500"/>
+        <Footer class="p-1 static bottom-0">
+            <FooterIcon href="https://github.com/ScidaJ/BestiaryURLConverter" target="_blank" rel="noopener noreferrer">
+                <GithubSolid class="w-5 h-5 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            </FooterIcon>
+        </Footer>
+    </div>
 </div>
